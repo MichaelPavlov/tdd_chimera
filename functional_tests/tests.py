ee3e5e278cdad6b37e62ecb24ada35d63d74fdc7
Michael Pavlov <mihpavlov@gmail.com>
2017-02-14 00:31:39 +0300

    Move functional test to app and use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,16 +1,14 @@
-import unittest
-from unittest import TestCase
-
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
-        # def tearDown(self):
-        # self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
@@ -20,7 +18,7 @@ class NewVisitorTest(TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about cool new online to-do app. She goes to check
         # out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -64,7 +62,3 @@ class NewVisitorTest(TestCase):
         # Satisfied, she goes back to sleep
 
         self.fail("Test incomplete")
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
