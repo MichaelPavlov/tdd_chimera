4162ef689fd5b56187840e72b7f5171248bb97ba
Michael Pavlov <mihpavlov@gmail.com>
2017-03-25 23:47:20 +0300

    Refactor list view to handle new item POSTs

--- tdd_chimera/urls.py
+++ tdd_chimera/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 
-from lists.views import home_page, view_list, new_list, add_item
+from lists.views import home_page, view_list, new_list
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
     url(r'^lists/new$', new_list, name='new_list'),
     url(r'^lists/(\d+)/$', view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', add_item, name='add_item$'),
+    # url(r'^lists/(\d+)/add_item$', add_item, name='add_item$'),
     # url(r'^admin/', admin.site.urls),
 ]
