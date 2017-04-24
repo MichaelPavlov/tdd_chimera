59afa14373d744a577a66fb3df66429e05145a43
Michael Pavlov <mihpavlov@gmail.com>
2017-04-24 11:18:19 +0300

    spiked in custom auth backend with persona

--- tdd_chimera/settings.py
+++ tdd_chimera/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -82,6 +83,11 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'accounts.ListUser'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PersonaAuthenticationBackend'
+)
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -118,3 +124,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        }
+    },
+    'root': {'level': 'INFO'}
+}
--- tdd_chimera/urls.py
+++ tdd_chimera/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from lists.views import home_page, view_list, new_list
 
@@ -21,6 +21,5 @@ urlpatterns = [
     url(r'^$', home_page, name='home'),
     url(r'^lists/new$', new_list, name='new_list'),
     url(r'^lists/(\d+)/$', view_list, name='view_list'),
-    # url(r'^lists/(\d+)/add_item$', add_item, name='add_item$'),
-    # url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls'))
 ]
