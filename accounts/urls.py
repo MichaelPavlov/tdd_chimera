692b3a09b10d64a790f3e52bb68d66a3fa044e86
Michael Pavlov <mihpavlov@gmail.com>
2017-05-07 22:42:22 +0300

    FT for login via email

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from accounts.views import send_login_email, login
-
-urlpatterns = [
-    url(r'^send_email$', send_login_email, name='send_login_email'),
-    url(r'^login$', login, name='login'),
-    url(r'^logout$', login, name='logout'),
-]
