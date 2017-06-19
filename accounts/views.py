cc75ab20d4999760f71d4665aafc0982b5f06920
Michael Pavlov <mihpavlov@gmail.com>
2017-06-19 12:59:52 +0300

    send_login_email to work

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,21 @@
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import redirect
+from django.urls import reverse
+
+from accounts.models import Token
 
 
 def send_login_email(request):
     email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('accounts:login') + '?token=' + str(token.uid)
+    )
+    message_body = 'Use this link to log in:\n\n{url}'.format(url=url)
     send_mail(
         'Your login link for Superlists',
-        'Use this link to log in.',
+        message_body,
         'noreply@superlists',
         [email]
     )
@@ -16,6 +24,7 @@ def send_login_email(request):
         "Check your email, we've sent you a link you can use to log in."
     )
     return redirect('/')
+
 
 def login(request):
     return redirect('/')
