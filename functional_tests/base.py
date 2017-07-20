decd4108d1426d948f962297d4a4f44cf4e16520
Michael Pavlov <mihpavlov@gmail.com>
2017-07-20 11:39:59 +0300

    functional_tests/base.py
    setUpClass, live_server_url

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -9,17 +10,35 @@ MAX_WAIT = 10
 
 
 class FunctionalTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_host = arg.split('=')[1]
+                cls.server_url = 'http://' + cls.server_host
+                cls.against_staging = True
+                return
+        super().setUpClass()
+        cls.against_staging = False
+        cls.server_url = cls.live_server_url
+
     def setUp(self):
         self.browser = webdriver.Chrome()
         staging_server = os.environ.get("STAGING_SERVER")
         if staging_server:
             setattr(self, 'live_server_url', 'http://' + staging_server)
+        pass
 
     def tearDown(self):
         self.browser.quit()
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
