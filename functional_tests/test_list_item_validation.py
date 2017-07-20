8b0c883b93953492d76d61f5e891d6e4126702b3
Michael Pavlov <mihpavlov@gmail.com>
2017-07-20 11:36:34 +0300

    use wait_for lambda assertion

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -19,7 +19,10 @@ class ItemValidationTest(FunctionalTest):
         # The home page refreshes, and there is one error message saying that list item
         # can not be blank.
         error = self.get_error_element()
-        self.assertEqual(error.text, "You can't have an empty list item")
+        self.wait_for(lambda: self.assertEqual(
+            self.get_error_element().text,
+            "You can't have an empty list item"
+        ))
 
         # She tries again with some text for the item, which now works.
         self.get_item_input_box().send_keys('Buy milk\n')
@@ -56,11 +59,15 @@ class ItemValidationTest(FunctionalTest):
         # Edith starts a new list in a way that causes a validation error
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys(Keys.ENTER)
-        error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertTrue(error.is_displayed())
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
 
         # She starts typing in the input box to clear error
         self.get_item_input_box().send_keys('a')
 
         # She is pleased to see that the error message disappears
-        self.assertFalse(error.is_displayed())
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
