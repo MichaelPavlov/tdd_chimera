2f2997e7e76b242778b2cc6909bb5a84cf380ffd
Michael Pavlov <mihpavlov@gmail.com>
2017-07-25 23:05:36 +0300

    user list url

--- lists/urls.py
+++ lists/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from lists.views import view_list, new_list
+from lists.views import view_list, new_list, my_lists
 
 urlpatterns = [
     url(r'^new$', new_list, name='new-list'),
     url(r'^(\d+)$', view_list, name='view-list'),
+    url(r'^users/(.+)$', my_lists, name='my-lists'),
 ]
