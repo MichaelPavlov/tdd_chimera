84d57ca63f9f4ab175caf58a1174862271b4014b
Michael Pavlov <mihpavlov@gmail.com>
2017-07-25 23:05:51 +0300

    moved create_pre_authenticated_session

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,12 +2,14 @@ import os
 import sys
 import time
 
+from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
-from functional_tests.server_tools import reset_database
+from functional_tests.management.commands.create_session import create_pre_authenticated_session
+from functional_tests.server_tools import reset_database, create_session_on_server
 
 MAX_WAIT = 10
 
@@ -92,3 +94,17 @@ class FunctionalTest(StaticLiveServerTestCase):
         )
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
+
+    def create_pre_authenticated_session(self, email):
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+        # to set a cookie we need to first visit the domain
+        # 404 pages load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/',
+        ))
